11eac2c6cecf086b9035ce05e386973b8da9a4fb
Ann Nelson <amnelson.work@gmail.com>
2025-04-27 22:56:47 -0400

    update slogan.js
    remove bouncing

--- client/script/slogan.js
+++ client/script/slogan.js
@@ -1,24 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const header = document.querySelector('h1');
-    const slogan = document.getElementById('slogan');
-
-    let position = 0;
-    let direction = 1;
-
-    function moveElements() {
-        position += direction * 2;
-
-        header.style.transform = `translateX(${position}px)`;
-        slogan.style.transform = `translateX(${position}px)`;
-
-        if (position > 100 || position < 0) {
-            direction *= -1;
-        }
-
-        requestAnimationFrame(moveElements);
-    }
-
-    moveElements();
+    const sloganElement = document.getElementById('slogan');
 
     const slogans = [
         "Art Made. Dreams Built",
@@ -26,11 +7,11 @@ document.addEventListener('DOMContentLoaded', function() {
         "Building Dreams, One Brush at a Time",
         "Designs that Inspire"
     ];
-    
+
     let sloganIndex = 0;
 
     setInterval(() => {
         sloganIndex = (sloganIndex + 1) % slogans.length;
-        slogan.innerHTML = `<em>${slogans[sloganIndex]}</em>`;
-    }, 3000); 
+        sloganElement.innerHTML = `<em>${slogans[sloganIndex]}</em>`;
+    }, 5000); 
 });
