ed8f3af7f027a43b6a752480187ddafdced69aef
Ann Nelson <amnelson.work@gmail.com>
2025-04-28 18:07:06 -0400

    update active js

--- client/script/active.js
+++ client/script/active.js
@@ -1,16 +1,13 @@
-
 const links = document.querySelectorAll('nav a');
 
-const currentPath = window.location.pathname;
-console.log("Current path:", currentPath); 
-
+const currentPath = window.location.pathname.split('/').pop(); 
+console.log("Current path:", currentPath);
 
 links.forEach((link) => {
-    const linkPath = new URL(link.href).pathname; 
-    console.log("Link path:", linkPath); 
-    
+    const linkPath = new URL(link.href).pathname.split('/').pop(); 
+    console.log("Link path:", linkPath);
+
     if (linkPath === currentPath) {
-        
         link.classList.add('active');
     }
 });
