aaa97ca722ac0e933aa9f27fd3b5aa9343ef94b1
Ann Nelson <amnelson.work@gmail.com>
2025-04-28 12:02:23 -0400

    update

---
+++ itis3135/scripts/sketch.js
@@ -0,0 +1,13 @@
+function setup() { 
+    // Creates a canvas 600 pixel W x 400 pixels H.
+    createCanvas(600, 400);
+}
+function draw() {
+    background(49, 107, 115);
+    fill("salmon"); //yellow
+    stroke("orange"); 
+    strokeWeight(20); 
+    circle(40, 50, 100); 
+}
+
+//step 7
