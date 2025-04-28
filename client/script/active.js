7ff44d964b9bc5b6f443f54d8f0c7a93ca964650
Ann Nelson <amnelson.work@gmail.com>
2025-04-27 23:17:56 -0400

    update active

--- client/script/active.js
+++ client/script/active.js
@@ -1,10 +1,16 @@
+
 const links = document.querySelectorAll('nav a');
 
 const currentPath = window.location.pathname;
+console.log("Current path:", currentPath); 
+
 
 links.forEach((link) => {
     const linkPath = new URL(link.href).pathname; 
+    console.log("Link path:", linkPath); 
+    
     if (linkPath === currentPath) {
+        
         link.classList.add('active');
     }
 });
